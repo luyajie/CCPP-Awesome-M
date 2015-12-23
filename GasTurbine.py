f762c334a525b498f98544ddae77ca33a4d1c9f5
Awesome-M <icesharp@gmail.com>
2015-12-23 14:48:32 +0800

    Modified Comments Styles

--- GasTurbine.py
+++ GasTurbine.py
@@ -6,11 +6,16 @@ from iapws import IAPWS97
 
 
 def air_enthalpy(t, rh):
-    """
+    """[summary]
 
-    :param t: 空气温度 ℃
-    :param rh: 相对湿度 %
-    :return: 空气焓值 [kJ/kg干]
+    [description]
+
+    Arguments:
+        t {[type]} -- [description]
+        rh {[type]} -- [description]
+
+    Returns:
+        [type] -- [description]
     """
     p_sat_stm = IAPWS97(T=t + 273.15, x=1).P * 1000000  # t温度下饱和压力 Pa
     p_stm = rh * p_sat_stm
@@ -21,13 +26,18 @@ def air_enthalpy(t, rh):
 
 
 def eff_comp(p_in, t_in, p_out, t_out):
-    """
-    计算燃机压气机效率
-    :param p_in: 入口压力
-    :param t_in: 入口温度 K
-    :param p_out:出口压力
-    :param t_out: 出口温度 K
-    :return:
+    """[summary]
+
+    [description]
+
+    Arguments:
+        p_in {[type]} -- [description]
+        t_in {[type]} -- [description]
+        p_out {[type]} -- [description]
+        t_out {[type]} -- [description]
+
+    Returns:
+        [type] -- [description]
     """
     t_out_s = pow(p_out / p_in, 0.2871) * t_in + 273.15
     print(t_out_s)
@@ -44,29 +54,6 @@ def eff_gt_rating(mw):
     eff_gt = -3.456e-6 * mw * mw + 0.002 * mw + 0.1102
     return eff_gt
 
-
-def ms_hrsg_rating(mw):
-    """主汽流量与燃机负荷
-    
-    已知燃机负荷，求主蒸汽流量。
-    
-    Arguments:
-        mw {[float]} -- 燃机负荷，单位：MW
-    """
-    pass
-
-
-def eff_gt(t, p, rh):
-    """[计算燃机效率]
-
-    [description]
-
-    Arguments:
-        t {[type]} -- [description]
-        p {[type]} -- [description]
-        rh {[type]} -- [description]
-    """
-    pass
 
 if __name__ == '__main__':
     test = eff_gt_rating(150)

0e63a7654f8b2e2293c01f6e8285df27107549ac
Awesome-M <icesharp@gmail.com>
2015-12-27 20:34:16 +0800

    Add Power Plant

---
+++ PowerPlant.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2015-12-26 21:59:24
+# @Author  : Your Name (you@example.org)
+# @Link    : http://example.org
+# @Version : $Id$
+
+import os
+
+"""
+#########################
+联合循环机组计算模块
+#########################
+"""
+print(__doc__)
+
+
+def load_dist(load):
+    """[summary]
+    
+    [description]
+    
+    Arguments:
+        load {[type]} -- [description]
+    
+    Returns:
+        [type] -- [description]
+    """
+    return x

79b01337cbda71cef4c14b2bf4162888b078da05
Awesome-M <icesharp@gmail.com>
2015-12-29 00:43:10 +0800

    Updated

--- dsync.py
+++ dsync.py
@@ -6,6 +6,20 @@
 # @Version : $Id$
 
 import subprocess
-op = subprocess.getoutput("rsync -vr /Users/LiuBing/Documents/CCPP-Awesome-M/ \
-/Users/LiuBing/PycharmProjects/CCPP-Awesome-M")
+opt = int(input("Please Choose your operation:"))
+"""
+1:从Github文件夹同步到编辑文件夹
+2:从编辑文件夹同步到Github文件夹
+"""
+print("1 or 2")
+if opt==1:
+    op = subprocess.getoutput("rsync -vr /Users/LiuBing/Documents/CCPP-Awesome-M/ "
+                              "/Users/LiuBing/PycharmProjects/CCPP-Awesome-M")
+elif opt==2:
+    op = subprocess.getoutput("rsync -vr "
+                              "/Users/LiuBing/PycharmProjects/CCPP-Awesome-M/ "
+                              "/Users/LiuBing/Documents/CCPP-Awesome-M")
+else:
+    print("reinput!")
 print(op)
+print("ur choose is %d"%(opt))
